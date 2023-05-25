ec20a68a75628a39b52ef37906de8e42cf38a1a4
biojuampa <104794253+biojuampa@users.noreply.github.com>
2023-05-25 20:30:42 -0300

    Otro commit más

---
+++ Curso_Python/tema2/saluda.py
@@ -0,0 +1,10 @@
+def saluda(name):
+    printf()
+    pritnf(f'Hola {name}, mucho gusto!')
+    printf()
+
+
+if __name__ == '__main__':
+    name = input('Dime tu nombre, por favor: ')
+    saluda(name)
+
